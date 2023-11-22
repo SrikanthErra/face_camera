77166a78c525c673e74dc2a10d51f06a1429fca0
swathi.p <swathi.p@cgg.gov.in>
2023-11-22 17:26:01 +0530

    anti spoofing

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
+import 'package:face_liveness/antiSpoofing.dart';
 import 'package:flutter/material.dart';
 
 import 'package:face_camera/face_camera.dart';
+import 'package:image/image.dart' as img;
+import 'package:tflite_flutter/tflite_flutter.dart' as tflite;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +24,15 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   File? _capturedImage;
+  Face? detectedFace;
+  File? cropSaveFile;
+  tflite.Interpreter? interpreter;
+
+  static const int INPUT_IMAGE_SIZE = 256;
+  static const double THRESHOLD = 0.8;
+  static const int LAPLACE_THRESHOLD = 50;
+  static const int LAPLACIAN_THRESHOLD = 250;
+  double? antiSpoofingScore;
 
   @override
   Widget build(BuildContext context) {
@@ -30,24 +42,37 @@ class _MyAppState extends State<MyApp> {
             title: const Text('FaceCamera example app'),
           ),
           body: Builder(builder: (context) {
-            if (_capturedImage != null) {
+            if (cropSaveFile != null) {
               return Center(
                 child: Stack(
                   alignment: Alignment.bottomCenter,
                   children: [
                     Image.file(
-                      _capturedImage!,
+                      cropSaveFile!,
                       width: double.maxFinite,
                       fit: BoxFit.fitWidth,
                     ),
-                    ElevatedButton(
-                        onPressed: () => setState(() => _capturedImage = null),
-                        child: const Text(
-                          'Capture Again',
-                          textAlign: TextAlign.center,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () =>
+                                setState(() => cropSaveFile = null),
+                            child: const Text(
+                              'Capture Again',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.w700),
+                            )),
+                        Text(
+                          "$antiSpoofingScore",
                           style: TextStyle(
-                              fontSize: 14, fontWeight: FontWeight.w700),
-                        ))
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ],
+                    )
                   ],
                 ),
               );
@@ -55,13 +80,15 @@ class _MyAppState extends State<MyApp> {
             return SmartFaceCamera(
                 autoCapture: true,
                 defaultCameraLens: CameraLens.front,
-                onCapture: (File? image) {
-                  
+                onCapture: (File? image) async {
                   setState(() => _capturedImage = image);
-                  print("Image captured $image");
+                  cropImage();
                 },
                 onFaceDetected: (Face? face) {
                   print("Face detected $face");
+                  setState(() {
+                    detectedFace = face;
+                  });
                   //Do something
                 },
                 messageBuilder: (context, face) {
@@ -84,4 +111,79 @@ class _MyAppState extends State<MyApp> {
             style: const TextStyle(
                 fontSize: 14, height: 1.5, fontWeight: FontWeight.w400)),
       );
+  cropImage() async {
+    img.Image capturedImage =
+        img.decodeImage(File(_capturedImage?.path ?? "").readAsBytesSync())!;
+    if (detectedFace != null && detectedFace!.boundingBox != null) {
+      img.Image faceCrop = img.copyCrop(
+        capturedImage,
+        x: detectedFace!.boundingBox.left.toInt() + 20,
+        y: detectedFace!.boundingBox.top.toInt() + 30,
+        width: detectedFace!.boundingBox.width.toInt(),
+        height: detectedFace!.boundingBox.height.toInt(),
+      );
+      final jpg = img.encodeJpg(faceCrop);
+      cropSaveFile = File(_capturedImage?.path ?? "");
+      await cropSaveFile?.writeAsBytes(jpg);
+      var laplacianScore = laplacian(cropSaveFile!);
+      if (laplacianScore < LAPLACIAN_THRESHOLD) {
+        print("Please place your face in the camera");
+      } else {
+        FaceAntiSpoofing faceAntiSpoofing = FaceAntiSpoofing();
+        antiSpoofingScore = await faceAntiSpoofing.loadModel(cropSaveFile);
+        print("antiSpoofingScoreeeeeeeeeeeee ${antiSpoofingScore}");
+        setState(() {});
+
+        if (antiSpoofingScore! < THRESHOLD) {
+          print("face recognised!!!!!!!!!!!!!!");
+        } else {
+          print("spoofing detected!!!!!!!!!!!!!!!!!");
+        }
+      }
+    }
+  }
+
+  int laplacian(File imageFile) {
+    img.Image capturedImage =
+        img.decodeImage(File(imageFile.path).readAsBytesSync())!;
+
+    // Size of the Laplacian filter
+    int score = 0;
+    const List<List<int>> laplace = [
+      [0, 1, 0],
+      [1, -4, 1],
+      [0, 1, 0],
+    ];
+    int size = laplace.length;
+    int height = capturedImage.height;
+    int width = capturedImage.width;
+
+    for (int x = 0; x < height - size + 1; x++) {
+      for (int y = 0; y < width - size + 1; y++) {
+        double result = 0;
+
+        // Convolution operation in the size x size region
+        for (int i = 0; i < size; i++) {
+          for (int j = 0; j < size; j++) {
+            img.Pixel pixelValue = getPixel(capturedImage, x + i, y + j);
+            result += pixelValue.r * laplace[i][j];
+            result += pixelValue.g * laplace[i][j];
+            result += pixelValue.b * laplace[i][j];
+          }
+        }
+        // Cast result to int before using it in the comparison
+        if (result.toInt() > LAPLACE_THRESHOLD) {
+          score++;
+        }
+      }
+    }
+    print("scoreeeeeeeeeeeeeeeeeeeeeeeeee ${score}");
+    return score;
+  }
+
+  img.Pixel getPixel(img.Image image, int x, int y) {
+    return image.getPixel(x, y);
+  }
+
+ 
 }
